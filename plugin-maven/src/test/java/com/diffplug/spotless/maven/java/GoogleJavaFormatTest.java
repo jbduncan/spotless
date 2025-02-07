4292af007bbd06817ac647f5e4e1755de25c41a7
Jonathan Bluett-Duncan <jonathan.bluett-dunc@sainsburys.co.uk>
2025-02-07 17:53:48 +0000

    WIP: Change default reflowLongStrings and reorderImports to true and fix some tests

--- plugin-maven/src/test/java/com/diffplug/spotless/maven/java/GoogleJavaFormatTest.java
+++ plugin-maven/src/test/java/com/diffplug/spotless/maven/java/GoogleJavaFormatTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2023 DiffPlug
+ * Copyright 2016-2025 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,7 +49,7 @@ class GoogleJavaFormatTest extends MavenIntegrationHarness {
 				"  <reflowLongStrings>true</reflowLongStrings>",
 				"</googleJavaFormat>");
 
-		runTest("java/googlejavaformat/JavaCodeFormattedReflowLongStrings.test");
+		runTest("java/googlejavaformat/JavaCodeFormattedReflowLongStringsDisabled.test");
 	}
 
 	@Test
